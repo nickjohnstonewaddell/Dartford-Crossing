4e019a5366499ea1d691bd63f62ef2126656f371
nickjohnstonewaddell <94445628+nickjohnstonewaddell@users.noreply.github.com>
2021-11-19 15:52:20 +0000

    Add files via upload

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Pay the Dartford Crossing charge (Dart Charge)',
+  serviceName: 'Pay for the Dartford Crossing (Dart Charge)',
 
   // Default port that prototype runs on
   port: '3000',
