59144419789616ac6c1c91328a90f5ac9d51fa8e
nickjohnstonewaddell <94445628+nickjohnstonewaddell@users.noreply.github.com>
2022-01-10 18:09:18 +0000

    Add files via upload

--- app/routes.js
+++ app/routes.js
@@ -3,4 +3,24 @@ const router = express.Router()
 
 // Add your routes here - above the module.exports line
 
+router.post('/landing-choice-a', function (req, res) {
+
+  var choice = req.session.data['dart-charge']
+
+  if (choice == "one-off"){
+    res.redirect('https://dart-charge-prototype.herokuapp.com/one-off-payment/v1.4a/pay-crossing')
+  }
+
+})
+
+router.post('/landing-choice-b', function (req, res) {
+
+  var choice = req.session.data['dart-charge']
+
+  if (choice == "one-off"){
+    res.redirect('https://dart-charge-prototype.herokuapp.com/one-off-payment/v1.4b/pay-crossing')
+  }
+
+})
+
 module.exports = router
